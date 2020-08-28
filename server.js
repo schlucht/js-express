8ee654329aaf3229baa8912b1a8aaeb5a247c91a
Schmid Lothar <lothi@schmidschlucht.ch>
2020-08-28 04:02:13 +0000

    install nodemon

--- server.js
+++ server.js
@@ -3,5 +3,15 @@ const express = require('express')
 
 const app = express()
 
+app.use((req, res, next) => {
+  console.log('Hallo Schmid Lothar')
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log('Hallo und nochmals dies ist noch eine')
+  res.send(`<h1>So endlich eine Ausgabe</h1>`)
+})
+
 http.createServer(app).listen(3000)
 
